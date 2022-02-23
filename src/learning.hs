ff1352ebd9a8f0c79b402ceadd2c3ec544391025
maximilian <maximilian.hagn@gmail.com>
2022-02-23 22:05:14 +0100

    Initial Commit

---
+++ src/learning.hs
@@ -0,0 +1,44 @@
+{-
+Commentary Section
+-}
+
+data Jahreszeiten = Fruehling | Sommer | Herbst | Winter
+type Vorname = [Char]
+type Nachname = [Char]
+data Geschlecht = Maennlich | Weiblich
+data Person = P Vorname Nachname Geschlecht
+
+func :: Integer -> Integer
+func n = 1
+
+fac :: Integer -> Integer
+fac n
+  | n == 0    = 1
+  | n > 0     = n * fac(n - 1)
+  | otherwise = error "PANIKMODUS"
+
+-- Hello World Implementation
+returnHelloWorld :: String -> String
+returnHelloWorld n = "Hello World, " ++ n ++ " : ) !"
+
+-- Quicksort Implementation
+quickSort :: [Integer] -> [Integer]
+quickSort [] = []
+quickSort (n:ns) = quickSort [m | m <- ns, m <= n]
+                   ++ [n]
+                   ++ quickSort [m | m <- ns, m > n]
+
+mergeSort :: [Integer] -> [Integer]
+mergeSort [] = []
+mergeSort (n:ns) = quickSort [m | m <- ns, m <= n]
+                  ++ [n]
+                  ++ quickSort [m | m <- ns, m > n]
+
+main = do
+  print ( show ( fac 1 ) )
+  print ( show ( fac 2 ) )
+  print ( show ( fac 3 ) )
+  print ( show ( fac 4 ) )
+  print ( show ( fac 5 ) )
+  print ( returnHelloWorld "Max" )
+  putStrLn ( returnHelloWorld "Max" )
